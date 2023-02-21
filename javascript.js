74ada51dea2cabc56acfbdee5d45b00e4604c489
sadng <115597343+sadng@users.noreply.github.com>
2023-02-21 09:27:25 -0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,102 @@
+var map = L.map('map').setView([47.258728, -122.465973], 13);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1Ijoic3FuZ3V5ZW4iLCJhIjoiY2w5eXd1YXc0MDk3MjNucDg2cDhyN3JrbyJ9.aMzoD2AZBPUtaVP2yV5N-A'
+}).addTo(map);
+
+var drawnItems = L.featureGroup().addTo(map);
+
+var tableData = L.layerGroup().addTo(map);
+
+var url = "https://178.128.228.240:4000/sql?q=";
+// change the Query below by replacing lab_7_name with your table name
+var sqlQuery = "SELECT geom, description, name FROM sadie_table";
+function addPopup(feature, layer) {
+    layer.bindPopup(
+        "<b>" + feature.properties.name + "</b><br>" +
+        feature.properties.description
+    );
+}
+
+fetch(url + sqlQuery)
+    .then(function(response) {
+    return response.json();
+    })
+    .then(function(data) {
+        L.geoJSON(data, {onEachFeature: addPopup}).addTo(tableData);
+    });
+
+new L.Control.Draw({
+    draw : {
+        polygon : true,
+        polyline : true,
+        rectangle : false,     // Rectangles disabled
+        circle : false,        // Circles disabled
+        circlemarker : false,  // Circle markers disabled
+        marker: true
+    },
+    edit : {
+        featureGroup: drawnItems
+    }
+}).addTo(map);
+
+function createFormPopup() {
+    var popupContent =
+        '<form>' +
+        'Description:<br><input type="text" id="input_desc"><br>' +
+        'User\'s Name:<br><input type="text" id="input_name"><br>' +
+        '<input type="button" value="Submit" id="submit">' +
+        '</form>'
+    drawnItems.bindPopup(popupContent).openPopup();
+}
+
+map.addEventListener("draw:created", function(e) {
+    e.layer.addTo(drawnItems);
+    createFormPopup();
+});
+
+function setData(e) {
+
+    if(e.target && e.target.id == "submit") {
+
+        // Get user name and description
+        var enteredUsername = document.getElementById("input_name").value;
+        var enteredDescription = document.getElementById("input_desc").value;
+
+        // Print user name and description
+        console.log(enteredUsername);
+        console.log(enteredDescription);
+
+        // Get and print GeoJSON for each drawn layer
+        drawnItems.eachLayer(function(layer) {
+            var drawing = JSON.stringify(layer.toGeoJSON().geometry);
+            console.log(drawing);
+        });
+
+        // Clear drawn items layer
+        drawnItems.closePopup();
+        drawnItems.clearLayers();
+
+    }
+}
+
+document.addEventListener("click", setData);
+
+map.addEventListener("draw:editstart", function(e) {
+    drawnItems.closePopup();
+});
+map.addEventListener("draw:deletestart", function(e) {
+    drawnItems.closePopup();
+});
+map.addEventListener("draw:editstop", function(e) {
+    drawnItems.openPopup();
+});
+map.addEventListener("draw:deletestop", function(e) {
+    if(drawnItems.getLayers().length > 0) {
+        drawnItems.openPopup();
+    }
+});
